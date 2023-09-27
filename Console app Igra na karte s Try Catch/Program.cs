4bb206d1f224ea53f1987e00609157db0d1ddc23
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-09-27 09:13:44 +0200

    Add files via upload

---
+++ Console app Igra na karte s Try Catch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_app_Igra_na_karte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int karte = 0;
+            int cardCounter = 0;
+
+
+            while (cardCounter < 31)
+            {
+                Console.WriteLine("Unesite broj karte od 1 - 13:  ");
+
+                try { 
+                karte = Convert.ToInt32(Console.ReadLine());
+                }
+                 catch (Exception greska) { Console.WriteLine("Pogrešan unos!"); 
+                Console.WriteLine(greska.Message);
+                }
+
+
+                if (karte <= 13 && karte >= 1)
+                {
+                    cardCounter += karte;
+                }
+
+                else Console.WriteLine("Unesi ispravnu kartu od 1 do 13.");
+            }
+
+
+            if (cardCounter == 31) 
+            {
+                Console.WriteLine("Pobjeda!");
+            }
+            else { Console.WriteLine("Gubitak!"); }
+
+
+
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
